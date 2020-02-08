a7a82b2072cae0daf07fc0ea025ee19586a9d21c
cody storch <wcstorch@gmail.com>
2020-02-08 02:57:40 -0700

    First Commit

--- Controllers/HarryVuController.cs
+++ Controllers/HarryVuController.cs
@@ -1,23 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+
 namespace BanTattoosNow.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class HarryVuController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-
-        [HttpGet]
+ [HttpGet]
         
         public IEnumerable<WeatherForecast> Get()
         {
